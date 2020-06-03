a82a89953c2c7917ec6ed5e055232cd4aa591593
anuragjha <toanuragjha@gmail.com>
2020-06-03 02:39:45 -0700

    enabled the RPC calls on the contracts pallet in Outer node

--- node/src/service.rs
+++ node/src/service.rs
@@ -29,6 +29,8 @@ macro_rules! new_full_start {
 	($config:expr) => {{
 		use std::sync::Arc;
 		use sp_consensus_aura::sr25519::AuthorityPair as AuraPair;
+		/*** Add This Line ***/
+        use jsonrpc_core::IoHandler;
 
 		let mut import_setup = None;
 		let inherent_data_providers = sp_inherents::InherentDataProviders::new();
@@ -78,7 +80,17 @@ macro_rules! new_full_start {
 				import_setup = Some((grandpa_block_import, grandpa_link));
 
 				Ok(import_queue)
-			})?;
+			})? // <- Remove semi-colon
+            /*** Add This Block ***/
+            .with_rpc_extensions(|builder| -> Result<IoHandler<sc_rpc::Metadata>, _> {
+                let handler = pallet_contracts_rpc::Contracts::new(builder.client().clone());
+                let delegate = pallet_contracts_rpc::ContractsApi::to_delegate(handler);
+
+                let mut io = IoHandler::default();
+                io.extend_with(delegate);
+                Ok(io)
+            })?;
+            /*** End Added Block ***/
 
 		(builder, import_setup, inherent_data_providers)
 	}}
